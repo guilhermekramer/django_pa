165a9a2d5f208d4f9f398b6744028493db333652
guilhermeKramer <guilherme_ok@hotmail.com>
2024-07-27 15:41:22 -0300

    blablalba

--- backend_pa/categories/models.py
+++ backend_pa/categories/models.py
@@ -4,7 +4,6 @@ from item.models import Item
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
     items = models.ManyToManyField( Item, related_name='categories')
 
     def __str__(self):
--- backend_pa/item/models.py
+++ backend_pa/item/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
